4f98cbe06f3e5dd916da85d6b807d5d2b102a970
nozzelozze <92754883+nozzelozze@users.noreply.github.com>
2024-12-10 22:29:54 +0100

    added favicon because why not

--- Server/src/index.php
+++ Server/src/index.php
@@ -4,7 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Slutprojekt</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png">
+    <link rel="manifest" href="/public/site.webmanifest">
+    <title>Balatro Runs</title>
     <link rel="stylesheet" href="./Style/style.css">
 </head>
 
