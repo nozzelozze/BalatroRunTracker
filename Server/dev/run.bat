a0ae5c6f01b0ed32f90ecf044708144c9f6f357b
nozzelozze <92754883+nozzelozze@users.noreply.github.com>
2024-12-10 10:53:56 +0100

    dev

--- Server/dev/run.bat
+++ Server/dev/run.bat
@@ -1,5 +1,5 @@
 @echo off
 for /f "tokens=1,2 delims==" %%A in (.env) do set %%A=%%B
 
-"%PHP_PATH%" -S localhost:%PORT%
+"%PHP_PATH%" -S localhost:%PORT% -t .\.\src\
 pause
--- Server/src/index.php
+++ Server/src/index.php
@@ -1,1 +1,6 @@
-Hello world
+<?php
+$servername = $_ENV["SQL_SERVERNAME"];
+$username = $_ENV["SQL_USERNAME"];
+$password = $_ENV["SQL_PASSWORD"];
+$dbname = $_ENV["SQL_DBNAME"];
+?>
