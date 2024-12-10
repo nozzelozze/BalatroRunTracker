2312731d664214d77483a82b7d3c7c5c8bc5b88e
nozzelozze <92754883+nozzelozze@users.noreply.github.com>
2024-12-10 21:34:42 +0100

    css

--- Server/src/index.php
+++ Server/src/index.php
@@ -1,9 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Slutprojekt</title>
+    <link rel="stylesheet" href="./Style/style.css">
+</head>
+
+<body>
+    <?php
+
     require "./Services/DBService.php";
     $result = DBService::getInstance()->getUsers();
 
     foreach ($result as $user) {
-        include "./Components/Usercard.php";
+        include "./components/Usercard.php";
     }
 
-?>
+    ?>
+</body>
+
+</html>
