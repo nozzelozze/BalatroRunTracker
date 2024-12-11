4488793fba5cc5add873789c32f5f176b8d5708e
nozzelozze <92754883+nozzelozze@users.noreply.github.com>
2024-12-11 01:26:51 +0100

    progress

---
+++ Server/src/Components/header.php
@@ -0,0 +1,11 @@
+<header class="header">
+    <div class="header__section">
+        <p>Balatro Runs</p>
+        <button>Runs</button>
+        <button>Leaderboards</button>
+    </div>
+    <div class="header__section">
+        <button>Log In</button>
+        <button>Sign Up</button>
+    </div>
+</header>
--- Server/src/index.php
+++ Server/src/index.php
@@ -1,28 +1,16 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png">
-    <link rel="manifest" href="/public/site.webmanifest">
-    <title>Balatro Runs</title>
-    <link rel="stylesheet" href="./Style/style.css">
-</head>
+ob_start();
 
-<body>
-    <?php
+require "./Services/DBService.php";
+$result = DBService::getInstance()->getUsers();
 
-    require "./Services/DBService.php";
-    $result = DBService::getInstance()->getUsers();
+foreach ($result as $user)
+{
+    include "./components/userCard.php";
+}
 
-    foreach ($result as $user) {
-        include "./components/Usercard.php";
-    }
+$content = ob_get_clean();
+include "layout.php";
 
-    ?>
-</body>
-
-</html>
+?>
