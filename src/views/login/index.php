ca5441797b355bb3673184eaa5584182a8eb0a4c
nozzelozze <92754883+nozzelozze@users.noreply.github.com>
2024-12-13 10:46:46 +0100

    login stuff

--- src/views/login/index.php
+++ src/views/login/index.php
@@ -1,1 +1,12 @@
-Login
+<div class="access-screen">
+    <h3>Login</h3>
+    <form id="login-form">
+        <input required type="text" name="Username" id="Username" placeholder="Username" />
+        <input required type="password" name="Password" id="Password" placeholder="Password" />
+
+        <button type="submit">Login</button>
+    </form>
+    <a href="/signup">
+        <p>or, signup</p>
+    </a>
+</div>
--- src/views/signup/index.php
+++ src/views/signup/index.php
@@ -1,1 +1,12 @@
-Sign up
+<div class="access-screen">
+    <h3>Sign Up</h3>
+    <form id="signup-form">
+        <input required type="text" name="Username" id="Username" placeholder="Username" />
+        <input required type="password" name="Password" id="Password" placeholder="Password" />
+
+        <button type="submit">Sign Up</button>
+    </form>
+    <a href="/login">
+        <p>or, login</p>
+    </a>
+</div>
