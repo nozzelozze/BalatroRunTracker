90249de72fb1cfc418a93e0a5527399c55c258bc
nozzelozze <92754883+nozzelozze@users.noreply.github.com>
2024-12-16 18:46:29 +0100

    better header

--- src/ui/components/header.php
+++ src/ui/components/header.php
@@ -1,19 +1,25 @@
 <header class="header">
     <div class="header__section">
-        <img src="assets/logo.webp" height="48px">
         <a href="/">
-            <p>Balatro Runs</p>
+            <img src="assets/logo.png" height="48px">
+        </a>
+        <a href="/">
+            <p class="header__logo-text">Balatro Runs</p>
         </a>
         <p><?= isset($_SESSION["Name"]) ? $_SESSION["Name"] : "" ?></p>
-        <a href="/"><button>Existing runs</button></a>
-        <a href="/new"><button>Submit run</button></a>
+        <a href="/"><button>Runs</button></a>
+        <a href="/new"><button>Submit</button></a>
     </div>
     <div class="header__section">
         <a href="/login">
-            <button>Log In</button>
+            <button class="button--black">Log In</button>
         </a>
         <a href="/signup">
-            <button>Sign Up</button>
+            <button class="button--black">Sign Up</button>
         </a>
+        <div class="user-badge">
+            <img src="assets/logoAlternate.webp" class="user-badge__avatar">
+            <span class="user-badge__name">nozzelozze</span>
+        </div>
     </div>
 </header>
